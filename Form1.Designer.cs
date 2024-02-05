d9bd1a75f354b5de9483eddee7939beabc6b6175
codejockey11 <junk_1105@hotmail.com>
2024-02-04 20:34:12 -0500

    Master Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace MyMetars
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxStation = new System.Windows.Forms.TextBox();
+            this.buttonGetMetar = new System.Windows.Forms.Button();
+            this.textBoxReport = new System.Windows.Forms.RichTextBox();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.labelUTC = new System.Windows.Forms.Label();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxStation
+            // 
+            this.textBoxStation.Location = new System.Drawing.Point(131, 13);
+            this.textBoxStation.Name = "textBoxStation";
+            this.textBoxStation.Size = new System.Drawing.Size(60, 20);
+            this.textBoxStation.TabIndex = 0;
+            this.textBoxStation.TextChanged += new System.EventHandler(this.textBoxStation_TextChanged);
+            // 
+            // buttonGetMetar
+            // 
+            this.buttonGetMetar.Location = new System.Drawing.Point(197, 11);
+            this.buttonGetMetar.Name = "buttonGetMetar";
+            this.buttonGetMetar.Size = new System.Drawing.Size(75, 23);
+            this.buttonGetMetar.TabIndex = 1;
+            this.buttonGetMetar.Text = "Get Metar";
+            this.buttonGetMetar.UseVisualStyleBackColor = true;
+            this.buttonGetMetar.Click += new System.EventHandler(this.buttonGet_Click);
+            // 
+            // textBoxReport
+            // 
+            this.textBoxReport.AcceptsTab = true;
+            this.textBoxReport.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxReport.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxReport.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxReport.Location = new System.Drawing.Point(13, 40);
+            this.textBoxReport.Name = "textBoxReport";
+            this.textBoxReport.ReadOnly = true;
+            this.textBoxReport.Size = new System.Drawing.Size(759, 397);
+            this.textBoxReport.TabIndex = 2;
+            this.textBoxReport.TabStop = false;
+            this.textBoxReport.Text = "";
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 440);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(784, 22);
+            this.statusStrip.TabIndex = 3;
+            this.statusStrip.Text = "statusStrip";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(74, 17);
+            this.toolStripStatusLabel.Text = "Enter Station";
+            // 
+            // labelUTC
+            // 
+            this.labelUTC.AutoSize = true;
+            this.labelUTC.Location = new System.Drawing.Point(12, 16);
+            this.labelUTC.Name = "labelUTC";
+            this.labelUTC.Size = new System.Drawing.Size(56, 13);
+            this.labelUTC.TabIndex = 2;
+            this.labelUTC.Text = "Date Time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 462);
+            this.Controls.Add(this.labelUTC);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.textBoxReport);
+            this.Controls.Add(this.buttonGetMetar);
+            this.Controls.Add(this.textBoxStation);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(800, 500);
+            this.Name = "Form1";
+            this.Text = "MyMetars";
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxStation;
+        private System.Windows.Forms.Button buttonGetMetar;
+        private System.Windows.Forms.RichTextBox textBoxReport;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.Label labelUTC;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.Xml;
+
+namespace MyMetars
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            FormatUtcLabel(null, null);
+            StartUtcLabelTimer();
+
+        }
+
+        private void FormatUtcLabel(object sender, EventArgs e)
+        {
+            String year = (DateTime.UtcNow.Year - 2000).ToString("D2");
+
+            this.labelUTC.Text = DateTime.UtcNow.Month.ToString("D2") + "/" + DateTime.UtcNow.Day.ToString("D2") + "/" + year + " " + DateTime.UtcNow.Hour.ToString("D2") + ":" + DateTime.UtcNow.Minute.ToString("D2") + ":" + DateTime.UtcNow.Second.ToString("D2") + "Z";
+
+        }
+
+        private void StartUtcLabelTimer()
+        {
+            Timer utcLabelTimer = new Timer();
+
+            utcLabelTimer.Interval = 1000;
+            utcLabelTimer.Tick += new EventHandler(FormatUtcLabel);
+            utcLabelTimer.Start();
+
+        }
+
+        private WebResponse MakeHttpRequest(String url)
+        {
+            try
+            {
+                WebRequest myRequest = WebRequest.Create(url);
+                myRequest.Method = "POST";
+                WebResponse response = myRequest.GetResponse();
+
+                return response;
+            }
+            catch (WebException err)
+            {
+                this.toolStripStatusLabel.Text = "WebException Error Occurred";
+                MessageBox.Show(err.Message);
+            }
+            catch (NotSupportedException err)
+            {
+                this.toolStripStatusLabel.Text = "NotSupportedException Error Occurred";
+                MessageBox.Show(err.Message);
+            }
+
+            return null;
+        }
+
+        private void AttributeWithTab(XmlAttribute a)
+        {
+            this.textBoxReport.AppendText("\t");
+            this.textBoxReport.AppendText(a.Name);
+            this.textBoxReport.AppendText(" ");
+            this.textBoxReport.AppendText(a.InnerText);
+            this.textBoxReport.AppendText(Environment.NewLine);
+        }
+
+        private void NodeWithTab(XmlNode n)
+        {
+            this.textBoxReport.AppendText("\t");
+            this.textBoxReport.AppendText(n.Name);
+            this.textBoxReport.AppendText(" ");
+            this.textBoxReport.AppendText(n.InnerText);
+            this.textBoxReport.AppendText(Environment.NewLine);
+        }
+
+        private void ElementAndInnerText(XmlElement e)
+        {
+            this.textBoxReport.AppendText(e.Name);
+
+            switch (e.Name)
+            {
+                case "temp_c":
+                case "dewpoint_c":
+                case "maxT_c":
+                case "minT_c":
+                    {
+                        this.textBoxReport.AppendText(" ");
+                        this.textBoxReport.AppendText(e.InnerText);
+                        this.textBoxReport.AppendText(" ");
+                        this.textBoxReport.AppendText(ConvertUnit.CelsiusToFahrenheit(Convert.ToDouble(e.InnerText)).ToString("F2"));
+                        this.textBoxReport.AppendText(Environment.NewLine);
+
+                        break;
+                    }
+
+                case "elevation_m":
+                    {
+                        this.textBoxReport.AppendText(" ");
+                        this.textBoxReport.AppendText(e.InnerText);
+                        this.textBoxReport.AppendText(" ");
+                        this.textBoxReport.AppendText(ConvertUnit.MeterToFeet(Convert.ToDouble(e.InnerText)).ToString("F2"));
+                        this.textBoxReport.AppendText(Environment.NewLine);
+
+                        break;
+                    }
+
+                case "quality_control_flags":
+                    {
+                        textBoxReport.AppendText(Environment.NewLine);
+                        foreach (XmlNode n in e.ChildNodes)
+                        {
+                            NodeWithTab(n);
+                        }
+
+                        break;
+                    }
+
+                default:
+                    {
+                        this.textBoxReport.AppendText(" ");
+                        this.textBoxReport.AppendText(e.InnerText);
+                        this.textBoxReport.AppendText(Environment.NewLine);
+
+                        break;
+                    }
+            }
+
+        }
+
+        private void FormatMetarText(XmlNodeList nodes)
+        {
+            if (nodes.Count > 0)
+            {
+                foreach (XmlNode node in nodes)
+                {
+                    String prevName = "";
+                    foreach (XmlElement element in node)
+                    {
+                        if (element.HasAttributes)
+                        {
+                            if (String.Compare(prevName, element.Name) != 0)
+                            {
+                                prevName = element.Name;
+                                this.textBoxReport.AppendText(element.Name);
+                                this.textBoxReport.AppendText(Environment.NewLine);
+                            }
+                            foreach (XmlAttribute attribute in element.Attributes)
+                            {
+                                AttributeWithTab(attribute);
+                            }
+                        }
+                        else
+                        {
+                            ElementAndInnerText(element);
+                        }
+                    }
+                    this.textBoxReport.AppendText(Environment.NewLine);
+                }
+
+                this.toolStripStatusLabel.Text = "Station Returned";
+                this.textBoxStation.Focus();
+            }
+            else
+            {
+                this.toolStripStatusLabel.Text = "No Data For Station";
+            }
+
+        }
+
+        private void FormatTafText(XmlNodeList nodes)
+        {
+            foreach (XmlNode node in nodes)
+            {
+                foreach (XmlElement element in node)
+                {
+                    if (element.Name == "forecast")
+                    {
+                        this.textBoxReport.AppendText(element.Name);
+                        this.textBoxReport.AppendText(Environment.NewLine);
+                        foreach (XmlNode n in element.ChildNodes)
+                        {
+                            if (n.Name == "sky_condition")
+                            {
+                                this.textBoxReport.AppendText("\t" + n.Name);
+                                this.textBoxReport.AppendText(Environment.NewLine);
+                                foreach (XmlAttribute a in n.Attributes)
+                                {
+                                    this.textBoxReport.AppendText("\t");
+                                    AttributeWithTab(a);
+                                }
+                            }
+                            else
+                            {
+                                NodeWithTab(n);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        ElementAndInnerText(element);
+                    }
+                }
+            }
+
+        }
+
+        private void buttonGet_Click(object sender, EventArgs e)
+        {
+            this.textBoxStation.Text = this.textBoxStation.Text.ToUpper();
+            this.textBoxReport.Clear();
+
+            WebResponse response = MakeHttpRequest("http://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=1&stationString=" + textBoxStation.Text);
+            if (response != null)
+            {
+                try
+                {
+                    XmlDocument doc = new XmlDocument();
+
+                    doc.Load(response.GetResponseStream());
+
+                    FormatMetarText(doc.SelectNodes("//METAR"));
+
+                    response.Close();
+                }
+                catch (WebException err)
+                {
+                    this.toolStripStatusLabel.Text = "WebException Error Occurred";
+                    MessageBox.Show(err.Message);
+                }
+                catch (XmlException err)
+                {
+                    this.toolStripStatusLabel.Text = "XML Error Occurred";
+                    MessageBox.Show(err.Message);
+                }
+
+            }
+
+            response = MakeHttpRequest("http://www.aviationweather.gov/adds/dataserver_current/httpparam?&dataSource=tafs&timeType=valid&requestType=retrieve&format=xml&mostRecent=true&hoursBeforeNow=1&stationString=" + textBoxStation.Text);
+            if (response != null)
+            {
+                try
+                {
+                    XmlDocument doc = new XmlDocument();
+
+                    doc.Load(response.GetResponseStream());
+
+                    FormatTafText(doc.SelectNodes("//TAF"));
+
+                    response.Close();
+                }
+                catch (WebException err)
+                {
+                    this.toolStripStatusLabel.Text = "WebException Error Occurred";
+                    MessageBox.Show(err.Message);
+                }
+                catch (XmlException err)
+                {
+                    this.toolStripStatusLabel.Text = "XML Error Occurred";
+                    MessageBox.Show(err.Message);
+                }
+
+            }
+
+
+        }
+
+        private void textBoxStation_TextChanged(object sender, EventArgs e)
+        {
+            if (this.textBoxStation.Text.Length == 4)
+            {
+                this.buttonGetMetar.Focus();
+            }
+
+        }
+
+    }
+
+    public class ConvertUnit
+    {
+        public static Double CelsiusToFahrenheit(Double c)
+        {
+            return ((9.0 / 5.0) * c) + 32;
+        }
+
+        public static Double FahrenheitToCelsius(Double f)
+        {
+            return (5.0 / 9.0) * (f - 32);
+        }
+
+        public static Double MeterToFeet(Double f)
+        {
+            return (f * 3.28084);
+        }
+
+        public static Double FeetToMeter(Double f)
+        {
+            return (f / 3.28084);
+        }
+    }
+}
